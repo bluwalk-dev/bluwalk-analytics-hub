c081dc6efcc345f6b8484de9a12074ccac505d9a
mpinto-bluwalk <67169217+mpinto-bluwalk@users.noreply.github.com>
2023-11-07 09:42:12 +0000

    hubspot staging improvements

--- models/reporting/facts/fct_calls.sql
+++ models/reporting/facts/fct_calls.sql
@@ -20,5 +20,5 @@ FROM (
     SELECT * FROM {{ ref('base_aircall_calls') }}
 ) a
 LEFT JOIN {{ ref('dim_employees') }} b ON a.agent_email = b.employee_email
-LEFT JOIN {{ ref('stg_hubspot__contacts') }} c ON a.contact_phone_nr = c.property_hs_calculated_phone_number
+LEFT JOIN {{ ref('stg_hubspot__contacts') }} c ON a.contact_phone_nr = c.hs_calculated_phone_number
 ORDER BY a.start_time DESC
