954c40e228ca97e73323b8607cb8db41c91eeb6b
mpinto@bluwalk.com <mpinto@bluwalk.com>
2023-11-09 12:49:13 +0000

    rideshare performance fix

--- models/reporting/facts/fct_user_rideshare_performance.sql
+++ models/reporting/facts/fct_user_rideshare_performance.sql
@@ -1,6 +1,7 @@
-
-SELECT * FROM {{ ref('base_uber_performance') }}
-UNION ALL
-SELECT * FROM {{ ref('base_bolt_performance') }}
-WHERE (online_minutes > 0 AND nr_trips > 0)
+SELECT * FROM (
+    SELECT * FROM {{ ref('base_uber_performance') }}
+    UNION ALL
+    SELECT * FROM {{ ref('base_bolt_performance') }}
+)
+WHERE (nr_trips > 0 AND online_minutes > 0)
 ORDER BY date DESC
