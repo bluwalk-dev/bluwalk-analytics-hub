e164f8667c143f658bec9d17676d312239a5831e
mpinto@bluwalk.com <mpinto@bluwalk.com>
2023-11-13 16:11:52 +0000

    fix on inc_sm_vehicles_active

--- models/incremental/inc_sm_vehicles_active.sql
+++ models/incremental/inc_sm_vehicles_active.sql
@@ -1,8 +1,10 @@
 SELECT
-    user_id,
-    vehicle_plate,
-    vehicle_brand_model,
-    vehicle_fuel_type,
-    vehicle_contract_type
-FROM {{ ref("dim_vehicle_contracts") }}
+    a.user_id,
+    b.email user_email,
+    a.vehicle_plate,
+    a.vehicle_brand_model,
+    a.vehicle_fuel_type,
+    a.vehicle_contract_type
+FROM {{ ref("dim_vehicle_contracts") }} a
+LEFT JOIN  {{ ref("dim_users") }} b ON a.user_id = b.user_id
 WHERE vehicle_contract_state = 'open'
