489057ac5f4312bc187d2487cb9840acc15ede1e
mpinto@bluwalk.com <mpinto@bluwalk.com>
2023-11-16 13:51:59 +0000

    improvements on deals

--- models/reporting/aggregates/activation/agg_activation_weekly_activation_points.sql
+++ models/reporting/aggregates/activation/agg_activation_weekly_activation_points.sql
@@ -2,9 +2,9 @@ SELECT
     b.year_week,
     c.start_date,
     c.end_date,
-    SUM(activation_point_score) activation_points
+    SUM(a.activation_point_score) activation_points
 FROM {{ ref("fct_deals") }} a
-LEFT JOIN {{ ref("util_calendar") }} b ON CAST(a.close_date AS DATE) = b.date
+LEFT JOIN {{ ref("util_calendar") }} b ON a.close_date = b.date
 LEFT JOIN {{ ref("util_week_intervals") }} c ON b.year_week = c.year_week
 WHERE 
     is_closed_won IS TRUE AND 
