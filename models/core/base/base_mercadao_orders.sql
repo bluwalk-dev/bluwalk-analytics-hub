69f18d62a97b758198853851436f12a9236f2619
mpinto@bluwalk.com <mpinto@bluwalk.com>
2023-11-16 23:00:59 +0000

    incremental partner_account_activated

--- models/core/base/base_mercadao_orders.sql
+++ models/core/base/base_mercadao_orders.sql
@@ -1,21 +1,29 @@
+-- Main query to retrieve order details along with partner and user information
 SELECT 
-    b.partner_id,
-    b.partner_name,
-    c.contact_id,
-    c.user_id,
-    c.name,
-    a.partner_account_uuid,
-    a.date,
-    a.nr_orders,
-    a.order_type,
+    b.partner_id,                  -- Partner ID from the partners accounts dimension
+    b.partner_name,                -- Partner name from the partners accounts dimension
+    c.contact_id,                  -- Contact ID from the users dimension
+    c.user_id,                     -- User ID from the users dimension
+    c.name,                        -- User's name from the users dimension
+    a.partner_account_uuid,        -- Partner account UUID from the subquery 'a'
+    a.date,                        -- Date of the order from the subquery 'a'
+    a.nr_orders,                   -- Number of orders from the subquery 'a'
+    a.order_type,                  -- Type of the order from the subquery 'a'
 FROM ( 
+    -- Subquery to get the latest order log for each partner account, date, and order type
     SELECT 
         *, 
-        ROW_NUMBER() OVER (PARTITION BY partner_account_uuid, date, order_type ORDER BY extraction_timestamp DESC) AS __row_number
-    FROM {{ ref("stg_mercadao__order_log") }} ) a
-LEFT JOIN {{ ref('dim_partners_accounts') }} b on a.partner_account_uuid = b.partner_account_uuid
-LEFT JOIN {{ ref('dim_users') }} c on c.contact_id = b.contact_id
+        ROW_NUMBER() OVER (
+            PARTITION BY partner_account_uuid, date, order_type 
+            ORDER BY extraction_timestamp DESC
+        ) AS __row_number
+    FROM {{ ref("stg_mercadao__order_log") }} 
+) a
+LEFT JOIN {{ ref('dim_partners_accounts') }} b 
+    ON a.partner_account_uuid = b.partner_account_uuid  -- Join with partners accounts dimension
+LEFT JOIN {{ ref('dim_users') }} c 
+    ON c.contact_id = b.contact_id                       -- Join with users dimension
 WHERE 
-    __row_number = 1 AND
-    date < current_date
-ORDER BY date DESC
+    __row_number = 1 AND                                  -- Filter to get the latest record per group
+    date < current_date                                  -- Filter to get records before the current date
+ORDER BY date DESC                                       -- Order results by date in descending order
---
+++ models/incremental/inc_wm_user_partner_account_activated.sql
@@ -0,0 +1,67 @@
+WITH 
+    -- CTE 'open_deals' to select open deals for the 'Work' marketplace
+    open_deals AS (
+        SELECT
+            user_id,           -- User ID from the deals fact table
+            partner_name       -- Partner name from the deals fact table
+        FROM {{ ref("fct_deals") }}
+        WHERE
+            is_closed = FALSE AND -- Filter for open deals
+            partner_marketplace = 'Work' -- Filter for 'Work' marketplace
+    ),
+    -- CTE 'activity_data' to gather user's first work date across various categories
+    activity_data AS (
+            -- Ridesharing activity data
+            SELECT
+                user_id,
+                partner_name,
+                MIN(CAST(request_timestamp AS DATE)) first_work_date -- Earliest ridesharing work date
+            FROM {{ ref("fct_user_rideshare_trips") }}
+            WHERE CAST(request_local_time AS DATE) > CAST(DATE_SUB(current_date(), INTERVAL 15 DAY) AS DATE)
+            GROUP BY user_id, partner_name
+
+            UNION ALL
+
+            -- Groceries activity data
+            SELECT
+                user_id,
+                'Mercadão' partner_name, -- Hardcoded partner name for groceries
+                MIN(date) first_work_date -- Earliest grocery order date
+            FROM {{ ref("base_mercadao_orders") }}
+            WHERE date > CAST(DATE_SUB(current_date, INTERVAL 15 DAY) AS DATE)
+            GROUP BY user_id, partner_name
+
+            UNION ALL
+
+            -- Parcel activity data
+            SELECT
+                user_id,
+                'Correos Express' partner_name, -- Hardcoded partner name for parcel
+                MIN(date) first_work_date -- Earliest parcel order date
+            FROM {{ ref("base_correos_express_orders") }}
+            WHERE date > CAST(DATE_SUB(current_date, INTERVAL 15 DAY) AS DATE)
+            GROUP BY user_id, partner_name
+
+            UNION ALL
+
+            -- Food Delivery activity data
+            SELECT
+                user_id,
+                partner_name,
+                MIN(end_date) first_work_date -- Earliest food delivery work date
+            FROM {{ ref("fct_user_job_orders") }}
+            WHERE
+                partner_category = 'Food Delivery' AND
+                end_date > CAST(DATE_SUB(CURRENT_DATE(), INTERVAL 15 DAY) AS DATE)
+            GROUP BY user_id, partner_name
+    )
+
+-- Final query to join open deals with the activity data
+SELECT
+  b.user_id,              -- User ID from the activity data
+  b.partner_name,         -- Partner name from the activity data
+  b.first_work_date       -- First work date from the activity data
+FROM open_deals a
+LEFT JOIN activity_data b
+ON a.partner_name = b.partner_name AND a.user_id = b.user_id
+WHERE b.user_id IS NOT NULL -- Filter to include only matched records
