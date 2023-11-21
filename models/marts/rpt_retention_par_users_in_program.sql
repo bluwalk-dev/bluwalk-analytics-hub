ae15aa4fedf35fba2f132335cd170734754e6bd9
mpinto@bluwalk.com <mpinto@bluwalk.com>
2023-11-21 11:23:34 +0000

    PAR users in program report

--- models/marts/rpt_retention_par_users_in_program.sql
+++ models/marts/rpt_retention_par_users_in_program.sql
@@ -1,10 +1,7 @@
 SELECT
-    hs_contact_id,
+    contact_id,
     first_name,
     last_name,
-    contact_phone_nr,
-    user_id,
-    contact_id,
     par_currently_in_program,
     par_days_in_program,
     par_entry_date,
@@ -14,5 +11,5 @@ SELECT
     par_number_of_days_with_earnings_between_70_and_130,
     par_sms_friday_challenge,
     mktplace_last_activity_ridesharing
-FROM {{ ref("stg_hubspot__contacts") }}
-WHERE is_deleted = FALSE
+FROM {{ ref("base_hubspot_contacts") }}
+WHERE par_currently_in_program = TRUE
--- models/staging/hubspot/stg_hubspot__contacts.sql
+++ models/staging/hubspot/stg_hubspot__contacts.sql
@@ -10,11 +10,22 @@ transformation as (
 
     SELECT
     
-        CAST(id AS INT64) id,
+        CAST(id AS INT64) hs_contact_id,
+        CAST(property_firstname AS STRING) first_name,
+        CAST(property_lastname AS STRING) last_name,
         CAST(is_deleted AS BOOL) is_deleted,
-        CAST(property_odoo_partner_id AS INT64) odoo_partner_id,
-        CAST(property_odoo_user_id AS INT64) odoo_user_id,
-        CAST(property_hs_calculated_phone_number AS STRING) hs_calculated_phone_number
+        CAST(property_odoo_partner_id AS INT64) contact_id,
+        CAST(property_odoo_user_id AS INT64) user_id,
+        CAST(property_hs_calculated_phone_number AS STRING) contact_phone_nr,
+        CAST(property_par_currently_in_program AS BOOL) par_currently_in_program,
+        CAST(property_par_days_in_program AS INT64) par_days_in_program,
+        CAST(property_par_entry_date AS DATE) par_entry_date,
+        CAST(property_par_exit_date AS DATE) par_exit_date,
+        CAST(property_par_number_days_with_0_earnings AS INT64) par_number_days_with_0_earnings,
+        CAST(property_par_number_of_days_with_earnings_below_70 AS INT64) par_number_of_days_with_earnings_below_70,
+        CAST(property_par_number_of_days_with_earnings_between_70_and_130 AS INT64) par_number_of_days_with_earnings_between_70_and_130,
+        CAST(property_par_sms_friday_challenge AS STRING) par_sms_friday_challenge,
+        CAST(property_mktplace_last_activity_ridesharing AS DATE) mktplace_last_activity_ridesharing
 
     FROM source
     WHERE _fivetran_deleted IS FALSE
