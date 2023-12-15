ee341cceb96bf207a7eedc3c38a466887fc911fa
mpinto@bluwalk.com <mpinto@bluwalk.com>
2023-12-15 00:18:03 +0000

    vehicle_documents fix

--- models/marts/rpt_customer_service_vehicle_documents.sql
+++ models/marts/rpt_customer_service_vehicle_documents.sql
@@ -1,9 +1,11 @@
 SELECT
+    c.user_vat,
+    c.user_name,
     'Bolt' partner,
-    a.*,
-    b.contact_id,
-    b.vehicle_contract_type
+    b.vehicle_contract_type,
+    a.*
 FROM {{ ref("base_bolt_documents") }} a
 LEFT JOIN {{ ref("dim_vehicle_contracts") }} b ON a.vehicle_plate = b.vehicle_plate
-WHERE b.end_date IS NULL AND contact_id IS NOT NULL
+LEFT JOIN {{ ref("dim_users") }} c ON b.contact_id = c.contact_id
+WHERE b.end_date IS NULL AND b.contact_id IS NOT NULL
 ORDER BY expires_timestamp ASC
