8699e1cdacf22583b2f841a8442bd83b8392ca66
mpinto@bluwalk.com <mpinto@bluwalk.com>
2023-12-19 17:22:50 +0000

    fixes in bolt and uber trips + retention report

--- models/core/base/base_freenow_trips.sql
+++ models/core/base/base_freenow_trips.sql
@@ -1,3 +1,11 @@
+WITH recent_vehicle_contracts AS (
+    SELECT *,
+           ROW_NUMBER() OVER (
+               PARTITION BY vehicle_plate
+               ORDER BY start_date DESC
+           ) AS rn
+    FROM {{ ref('dim_vehicle_contracts') }}
+)
 SELECT
     upa.partner_key,
     upa.sales_partner_id,
@@ -19,7 +27,7 @@ SELECT
 from {{ ref('stg_freenow__trips') }} ta
 LEFT JOIN {{ ref('dim_partners_accounts') }} upa on ta.partner_account_uuid = upa.partner_account_uuid
 LEFT JOIN {{ ref('dim_users') }} u on u.contact_id = upa.contact_id
-LEFT JOIN {{ ref('dim_vehicle_contracts') }} z ON ta.vehicle_plate = z.vehicle_plate
+LEFT JOIN recent_vehicle_contracts z ON ta.vehicle_plate = z.vehicle_plate AND z.rn = 1
 WHERE 
     ta.request_timestamp < CAST(IFNULL(z.end_date, current_date) as TIMESTAMP) AND 
     ta.request_timestamp > CAST(z.start_date AS TIMESTAMP) AND
