a9436c99d769df1f504cd29ec6aad1c66f565655
mpinto@bluwalk.com <mpinto@bluwalk.com>
2024-02-23 16:08:42 +0000

    event list fix

--- models/marts/insights/rpt_insights_events_list.sql
+++ models/marts/insights/rpt_insights_events_list.sql
@@ -8,7 +8,8 @@ SELECT
     event_end_date,
     event_category,
     event_labels,
-    event_phq_attendance,
+    expected_attendance,
     event_navigation_link
 FROM {{ ref("base_predict_hq_events") }}
 WHERE event_phq_attendance > 1000
+ORDER BY event_start_date ASC
