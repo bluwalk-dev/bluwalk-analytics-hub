a08ab2945b2dfb94899badc59ae707d86043cdd8
mpinto@bluwalk.com <mpinto@bluwalk.com>
2024-03-04 15:42:31 +0000

    improvements on risk profile

--- models/incremental/inc_fin_risk_profile.sql
+++ models/incremental/inc_fin_risk_profile.sql
@@ -36,6 +36,7 @@ FROM (
             ELSE NULL
         END as next_installment,
         a.net_balance,
+        a.accounting_balance
     FROM {{ ref('rpt_finances_collections_debt_report') }} a
     LEFT JOIN {{ ref('dim_users') }} b ON a.contact_id = b.contact_id
     LEFT JOIN open_vehicle_contracts c ON b.user_id = c.user_id
@@ -51,5 +52,6 @@ WHERE
         ROUND(IFNULL(y.risk_deposit_amount, 0), 2) != ROUND(IFNULL(x.deposit, 0), 2) OR
         ROUND(IFNULL(y.risk_net_balance, 0), 2) != ROUND(IFNULL(x.net_balance, 0), 2) OR
         ROUND(IFNULL(y.risk_next_installment, 0), 2) != ROUND(IFNULL(x.next_installment, 0), 2) OR
-        ROUND(IFNULL(y.risk_target_balance, 0), 2) != ROUND(IFNULL(x.target_balance, 0), 2)
+        ROUND(IFNULL(y.risk_target_balance, 0), 2) != ROUND(IFNULL(x.target_balance, 0), 2) OR
+        ROUND(IFNULL(y.risk_accounting_balance, 0), 2) != ROUND(IFNULL(x.accounting_balance, 0), 2)
     )
