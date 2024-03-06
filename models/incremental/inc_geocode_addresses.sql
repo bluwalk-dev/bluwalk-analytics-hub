40c09bc8e589594e858ebeecba52de97ace6d8d5
mpinto@bluwalk.com <mpinto@bluwalk.com>
2024-03-06 22:46:09 +0000

    incremental for geocoding

---
+++ models/incremental/inc_geocode_addresses.sql
@@ -0,0 +1,11 @@
+SELECT
+    a.address_pickup,
+    COUNT(*) count
+FROM {{ ref('fct_user_rideshare_trips') }} a
+LEFT JOIN {{ ref('stg_bwk_insights__address_geocoding') }} b ON a.address_pickup = b.address
+WHERE
+    a.address_pickup_zip IS NULL AND
+    b.address IS NULL
+GROUP BY a.address_pickup
+ORDER BY count DESC
+LIMIT 100
