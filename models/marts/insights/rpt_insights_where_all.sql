2e80409f52d6e70197495a794d142857d760a689
mpinto@bluwalk.com <mpinto@bluwalk.com>
2024-03-14 17:15:39 +0000

    insights fixes

--- models/marts/insights/rpt_insights_where_all.sql
+++ models/marts/insights/rpt_insights_where_all.sql
@@ -1,10 +1,21 @@
+WITH ranked_zones AS (
+  SELECT
+    location_id,
+    zone_name,
+    zone_navigation_link,
+    SUM(nr_of_trips) AS nr_of_trips,
+    ROW_NUMBER() OVER(PARTITION BY location_id ORDER BY SUM(nr_of_trips) DESC) as rank,
+    MAX(SUM(nr_of_trips)) OVER(PARTITION BY location_id) as max_trips_per_location
+  FROM {{ ref('int_insights_trips_last_30_days') }}
+  GROUP BY location_id, zone_name, zone_navigation_link
+)
+
 SELECT
   location_id,
   zone_name,
   zone_navigation_link,
-  SUM(nr_of_trips) AS nr_of_trips,
-  ROUND((SUM(nr_of_trips) / MAX(SUM(nr_of_trips)) OVER ())*100) as percentage
-FROM {{ ref('int_insights_trips_last_30_days') }}
-GROUP BY location_id, zone_name, zone_navigation_link
-ORDER BY nr_of_trips DESC
-LIMIT 10
+  nr_of_trips,
+  ROUND((nr_of_trips / max_trips_per_location) * 100) as percentage
+FROM ranked_zones
+WHERE rank <= 10
+ORDER BY location_id, rank
