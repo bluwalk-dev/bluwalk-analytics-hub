d4e8f16de4b9847f465f3da07b4cac58d095aac0
mpinto@bluwalk.com <mpinto@bluwalk.com>
2024-03-26 12:06:05 +0000

    document management

---
+++ models/core/base/base_bolt_vehicle_documents.sql
@@ -0,0 +1,15 @@
+WITH last_version_documents AS (
+    SELECT MAX(load_timestamp) last_version
+    FROM {{ ref("stg_bolt__expiring_documents") }} 
+)
+
+SELECT
+   REPLACE(identifier, '-', '') vehicle_plate,
+   company_id,
+   type_title document_name,
+   entity_id,
+   expiration_date
+FROM {{ ref("stg_bolt__expiring_documents") }}
+WHERE 
+    load_timestamp = (SELECT * FROM last_version_documents) AND
+    entity_type = 'car'
