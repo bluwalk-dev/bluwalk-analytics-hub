2a7db042ace910320e75c8c6544d3be1309b6455
mpinto@bluwalk.com <mpinto@bluwalk.com>
2024-04-30 00:31:36 +0000

    advertising reports

---
+++ models/marts/advertising/rpt_advertising_google_ads_account_created.sql
@@ -0,0 +1,21 @@
+WITH account_created_events AS (
+    SELECT
+        *
+    FROM (
+        SELECT *, ROW_NUMBER() OVER (PARTITION BY id ORDER BY loaded_at DESC) AS __row_number 
+        FROM {{ ref('stg_segment__account_created') }}
+    ) a
+    LEFT JOIN {{ ref('dim_users') }} b ON CAST(a.user_id AS INT64) = b.user_id
+    WHERE __row_number = 1
+    ORDER BY a.original_timestamp DESC
+)
+
+SELECT
+  UNIX_MICROS(original_timestamp) as conversion_event_time,
+  gclid,
+  context_email AS email,
+  user_phone AS phone_number,
+  1 conversion_value,
+  'EUR' currency_code
+FROM account_created_events
+WHERE gclid IS NOT NULL
