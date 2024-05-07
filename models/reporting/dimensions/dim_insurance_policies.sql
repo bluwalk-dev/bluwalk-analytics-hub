f5c2733745cabef1a17173e30e92985be12a6e65
mpinto@bluwalk.com <mpinto@bluwalk.com>
2024-05-07 10:10:10 +0000

    insurance renewal changes

---
+++ models/reporting/dimensions/dim_insurance_policies.sql
@@ -0,0 +1,15 @@
+SELECT 
+    a.id as insurance_policy_id,  -- Unique identifier for the vehicle insurance record
+    c.name insurance_type,  -- Type of insurance policy
+    c.policy_class insurance_class,  -- Type of insurance policy
+    a.auto_vehicle_id insurance_vehicle_id,  -- Identifier for the associated vehicle
+    a.insurer_id as insurer_contact_id,  -- Identifier for the insurer contact
+    b.contact_full_name as insurer_name,  -- Full name of the insurer
+    a.policy_number insurance_policy_number,  -- Policy number of the insurance
+    a.start_date insurance_start_date,  -- Start date of the insurance policy
+    a.renewal_date insurance_renewal_date,  -- End date of the insurance policy
+    a.state insurance_state  -- Current state of the insurance policy
+
+FROM {{ ref('stg_odoo__insurance_policies') }} a  -- Source table: staged fleet vehicle insurances data
+LEFT JOIN {{ ref('dim_contacts') }} b ON a.insurer_id = b.contact_id  -- Joining with contacts dimension table for insurer details
+LEFT JOIN {{ ref('stg_odoo__insurance_policy_types') }} c ON a.policy_type_id = c.id  -- Joining with contacts dimension table for insurer details
