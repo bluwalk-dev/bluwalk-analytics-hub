8441ed772549c625881002af2d39485da2abdcfb
mpinto@bluwalk.com <mpinto@bluwalk.com>
2024-06-06 23:58:15 +0000

    ga4 push

--- models/staging/google_analytics/stg_google_analytics__ga4_events.sql
+++ models/staging/google_analytics/stg_google_analytics__ga4_events.sql
@@ -1,14 +1,16 @@
-with
+{{ config(
+    materialized='incremental',
+    unique_key=['event_name', 'event_timestamp', 'user_pseudo_id']
+) }}
 
-    source as (select * from {{ source('analytics_323684004', 'events_*') }}),
+SELECT
+    *
+FROM
+    {{ ref('stg_google_analytics__ga4_events_t') }}
+WHERE event_date BETWEEN '20220925' AND '20240604'
 
-    transformation as (
-
-        SELECT
-            *
-        FROM source
-
-    )
-
-select *
-from transformation
+/*
+{% if is_incremental() %}
+WHERE
+    event_timestamp > (SELECT MAX(event_timestamp) FROM {{ this }})
+{% endif %}*/
--- models/staging/google_analytics/stg_google_analytics__ga4_merged_events.sql
+++
@@ -1,9 +0,0 @@
-{{ config(
-    materialized='incremental'
-) }}
-
-SELECT
-    *
-FROM
-    {{ ref('stg_google_analytics__ga4_events') }}
-WHERE event_date BETWEEN '20220925' AND '20240604'
