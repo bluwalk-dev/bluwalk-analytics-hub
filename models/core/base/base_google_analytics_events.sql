4f6341f817f637b2e2aa26559b28764a90d7c8c1
mpinto@bluwalk.com <mpinto@bluwalk.com>
2024-06-07 01:25:57 +0000

    ga4 final final

--- models/core/base/base_google_analytics_events.sql
+++ models/core/base/base_google_analytics_events.sql
@@ -7,22 +7,41 @@
     }
 ) }}
 
-SELECT * FROM (
-    SELECT
-        PARSE_DATE('%Y%m%d', event_date) AS event_date,
-        * EXCEPT(event_date)
-    FROM {{ ref('stg_google_analytics__ga342899811_events') }}
-    WHERE event_date > '20240605'
+WITH base AS (
+    {% if is_incremental() %}
+        SELECT
+            PARSE_DATE('%Y%m%d', event_date) AS event_date,
+            * EXCEPT(event_date)
+        FROM {{ ref('stg_google_analytics__ga342899811_events') }}
+        WHERE event_date > '20240605'
+          AND event_timestamp > (SELECT MAX(event_timestamp) FROM {{ this }})
 
-    UNION ALL
+        UNION ALL
 
-    SELECT
-        PARSE_DATE('%Y%m%d', event_date) AS event_date,
-        * EXCEPT(event_date)
-    FROM {{ ref('stg_google_analytics__ga323684004_events') }}
-    WHERE event_date < '20240605'
+        SELECT
+            PARSE_DATE('%Y%m%d', event_date) AS event_date,
+            * EXCEPT(event_date)
+        FROM {{ ref('stg_google_analytics__ga323684004_events') }}
+        WHERE event_date < '20240605'
+          AND event_timestamp > (SELECT MAX(event_timestamp) FROM {{ this }})
+
+    {% else %}
+        SELECT
+            PARSE_DATE('%Y%m%d', event_date) AS event_date,
+            * EXCEPT(event_date)
+        FROM {{ ref('stg_google_analytics__ga342899811_events') }}
+        WHERE event_date > '20240605'
+
+        UNION ALL
+
+        SELECT
+            PARSE_DATE('%Y%m%d', event_date) AS event_date,
+            * EXCEPT(event_date)
+        FROM {{ ref('stg_google_analytics__ga323684004_events') }}
+        WHERE event_date < '20240605'
+    {% endif %}
 )
 
-{% if is_incremental() %}
-    WHERE event_timestamp > (SELECT MAX(event_timestamp) FROM {{ this }})
-{% endif %}
+SELECT
+    *
+FROM base
