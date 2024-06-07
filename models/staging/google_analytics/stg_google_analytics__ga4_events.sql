7fbbdb8c4d267b0b8d2a58d00b0b6964bbf8af68
mpinto@bluwalk.com <mpinto@bluwalk.com>
2024-06-07 00:21:23 +0000

    another ga4 fix

--- models/staging/google_analytics/stg_google_analytics__ga4_events.sql
+++ models/staging/google_analytics/stg_google_analytics__ga4_events.sql
@@ -1,19 +1,19 @@
 {{ config(
     materialized='incremental',
-    unique_key=['event_name', 'event_timestamp', 'user_pseudo_id'],
+    unique_key=['event_timestamp', 'user_pseudo_id'],
     partition_by={
-        'field': 'CAST(event_date AS DATE)',
+        'field': 'event_date_parsed',
         'data_type': 'DATE'
     }
 ) }}
 
 SELECT
-    *
+    *,
+    PARSE_DATE('%Y%m%d', event_date) AS event_date_parsed  -- Ensure event_date is parsed to DATE
 FROM
-    {{ ref('stg_google_analytics__ga4_events_t') }}
-WHERE event_date BETWEEN '20220925' AND '20240604'
+    {{ ref('base_google_analytics_4_page_views') }}
 
 {% if is_incremental() %}
 WHERE
-    event_timestamp > (SELECT MAX(event_timestamp) FROM {{ this }})
+    event_date > (SELECT MAX(event_date) FROM {{ this }})
 {% endif %}
