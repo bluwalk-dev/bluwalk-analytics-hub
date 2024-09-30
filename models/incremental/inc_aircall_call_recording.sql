8c75759db6289f84a82b9f4e1cf2cc7da0da264f
mpinto@bluwalk.com <mpinto@bluwalk.com>
2024-09-30 22:26:55 +0000

    update to call records inc

--- models/incremental/inc_aircall_call_recording.sql
+++ models/incremental/inc_aircall_call_recording.sql
@@ -1,5 +1,6 @@
 SELECT
-    a.db_id AS id
+    a.db_id AS id,
+    ended_at
 FROM {{ ref('int_aircall_call_ended') }} a 
 LEFT JOIN {{ ref('stg_aircall__call_recordings') }} b 
     ON a.db_id = b.id
