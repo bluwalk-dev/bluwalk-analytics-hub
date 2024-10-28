c252beb2dd71f7b5f544deaad20c030b2f3f223e
mpinto@bluwalk.com <mpinto@bluwalk.com>
2024-10-28 12:56:17 +0000

    error in bolt trips

--- models/core/base/base_bolt_trips.sql
+++ models/core/base/base_bolt_trips.sql
@@ -23,6 +23,6 @@ LEFT JOIN {{ ref('dim_users') }} u on u.contact_id = upa.contact_id
 LEFT JOIN {{ ref('dim_vehicle_contracts') }} z ON ta.vehicle_plate = z.vehicle_plate
 WHERE 
     (ta.accepted_time BETWEEN CAST(z.start_date AS TIMESTAMP) AND 
-    CAST(IFNULL(z.end_date, current_date) as TIMESTAMP) OR z.vehicle_plate IS NULL) AND
+    CAST(IFNULL(z.end_date, current_date) as TIMESTAMP) OR z.vehicle_plate IS NOT NULL) AND
     order_state = 'finished'
 ORDER BY accepted_time DESC
