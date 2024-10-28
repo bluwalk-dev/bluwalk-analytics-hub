d402fd4b645b9e11481da7ced4a0aad4e024ac05
mpinto@bluwalk.com <mpinto@bluwalk.com>
2024-10-28 22:28:56 +0000

    transaction account forwarding

---
+++ models/staging/odoo/stg_odoo__transaction_accounts.sql
@@ -0,0 +1,27 @@
+{{ config(materialized='table') }}
+
+with
+
+source as (
+    select
+        *
+    from {{ source('odoo_realtime', 'transaction_account') }}
+),
+
+transformation as (
+
+    select
+        
+        id as transaction_account_id,
+        name as transaction_account_name,
+        user_id,
+        partner_email as contact_email,
+        partner_nif as contact_vat,
+        type as transaction_account_type,
+
+
+    from source
+    
+)
+
+select * from transformation
