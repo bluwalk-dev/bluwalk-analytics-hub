f9d5ac4049a5a853ac177852dd90a42b5f3d658c
mpinto@bluwalk.com <mpinto@bluwalk.com>
2024-11-05 19:39:46 +0000

    quick fix

--- models/marts/finances/imports/rpt_finances_prio_import.sql
+++ models/marts/finances/imports/rpt_finances_prio_import.sql
@@ -15,7 +15,7 @@ FROM {{ ref("base_prio_charging_sessions") }} a
 LEFT JOIN {{ ref("dim_energy_cards") }} b ON a.card_name = b.card_name AND CAST(a.start_timestamp AS TIMESTAMP) BETWEEN CAST(b.delivery_date AS TIMESTAMP) AND CAST(COALESCE(b.receive_date, CURRENT_DATE()) AS TIMESTAMP)
 LEFT JOIN {{ ref("rpt_finances_service_import_helper") }} c ON b.contact_id = c.contact_id
 WHERE 
-    energy_id is null AND
-    start_date BETWEEN 
+    a.energy_id is null AND
+    a.start_date BETWEEN 
         CAST('2024-01-01 00:00:00' AS DATETIME) AND DATETIME_SUB(DATETIME(DATE_ADD(DATE_TRUNC(CURRENT_DATE(), WEEK), INTERVAL 1 DAY)), INTERVAL 1 SECOND) AND
-    card_name IS NOT NULL
+    a.card_name IS NOT NULL
