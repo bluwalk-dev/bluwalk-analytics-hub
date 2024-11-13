bfb2be89cad64aa18a2d6803f7248339804d35ac
mpinto@bluwalk.com <mpinto@bluwalk.com>
2024-11-13 09:21:27 +0000

    fix on fuel

--- models/core/base/base_service_orders_fuel.sql
+++ models/core/base/base_service_orders_fuel.sql
@@ -6,7 +6,9 @@ SELECT
     b.user_id,
     a.contact_id,
     a.card_name,
+    a.start_date_ts,
     a.start_date,
+    a.end_date_ts,
     a.end_date,
     a.energy_source,
     a.station_name,
@@ -17,7 +19,6 @@ SELECT
     a.cost,
     a.margin,
     a.statement
-    
 FROM {{ ref('stg_odoo__fuel') }} a
 LEFT JOIN {{ ref('dim_users') }} b ON a.contact_id = b.contact_id
 LEFT JOIN {{ ref('dim_partners') }} c ON a.service_partner_id = c.service_partner_id
