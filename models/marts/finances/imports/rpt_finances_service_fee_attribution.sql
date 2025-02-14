a19fe1a214ea835fbb61019328a2d9c9431a99ad
mpinto@bluwalk.com <mpinto@bluwalk.com>
2025-02-14 18:11:35 +0000

    service fee attribution

--- models/marts/finances/imports/rpt_finances_service_fee_attribution.sql
+++ models/marts/finances/imports/rpt_finances_service_fee_attribution.sql
@@ -7,7 +7,7 @@ SELECT
     vehicle_plate,
     payee_is_company,
     CASE
-        WHEN service_fee = 5 AND vehicle_plate IN ('20VX96', 'AT35NN', 'AX33LC') AND vehicle_contract_type = 'free_loan' THEN TRUE
+        WHEN service_fee = 5 AND vehicle_plate IN ('20VX96', 'AT35NN', 'AX33LC', 'BC39HN') AND vehicle_contract_type = 'free_loan' THEN TRUE
         WHEN service_fee = 5 AND vehicle_contract_type = 'free_loan' AND payee_is_company = FALSE THEN FALSE
         ELSE TRUE
     END as service_fee_compliant
