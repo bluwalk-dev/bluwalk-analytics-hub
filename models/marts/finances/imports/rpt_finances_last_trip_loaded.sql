b13e0e687e4bcce41f6cd30b9132e383b76e7c37
mpinto@bluwalk.com <mpinto@bluwalk.com>
2025-03-27 23:35:33 +0000

    last trip loaded

--- models/marts/finances/imports/rpt_finances_last_trip_loaded.sql
+++ models/marts/finances/imports/rpt_finances_last_trip_loaded.sql
@@ -2,7 +2,7 @@ SELECT
     partner_name,
     partner_login_location,
     max(request_local_time)
-FROM {{ ref('fct_user_rideshare_trips') }} a
+FROM {{ ref('fct_user_rideshare_trips') }}
 WHERE 
     partner_name IS NOT NULL AND
     partner_login_location IS NOT NULL
