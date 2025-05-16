8aea00691de5476e181cfc2aadaa13d5ebf96f82
mpinto@bluwalk.com <mpinto@bluwalk.com>
2025-05-16 11:40:26 +0000

    impact on deletion of rideshare hubspot pipelines

--- models/marts/datastudio/dm_datastudio_daily.sql
+++ models/marts/datastudio/dm_datastudio_daily.sql
@@ -10,30 +10,20 @@ SELECT
     a.date,
     a.year_week,
     a.year_month,
-    -- Table values Marketing
-    a.marketing_points,
-    a.nr_signups,
-    a.new_food_accounts,
-    a.new_rideshare_accounts,
-    a.new_shopping_accounts,
-    a.new_parcel_accounts,
-    a.new_total_accounts,
-    -- Table values Activation
-    b.nr_missed_calls act_nr_missed_calls,
-    b.nr_inbound_calls act_nr_inbound_calls,
-    b.missed_call_ratio act_missed_call_ratio,
+    a.nr_missed_calls act_nr_missed_calls,
+    a.nr_inbound_calls act_nr_inbound_calls,
+    a.missed_call_ratio act_missed_call_ratio,
     -- Table values Customer Service
-    c.number_of_ratings,
-    c.average_rating,
-    c.nr_tickets_first_reply_time,
-    c.first_reply_time,
-    c.nr_missed_calls,
-    c.nr_inbound_calls,
-    c.missed_call_ratio,
-    c.retention_success,
-    c.retention_attempts,
-    c.nr_tickets_resolution_time,
-    c.resolution_time
-FROM {{ ref('rpt_marketing_daily_kpis') }} a
-LEFT JOIN {{ ref('rpt_activation_daily_kpis') }} b ON a.date = b.date
-LEFT JOIN {{ ref('rpt_customer_service_daily_kpis') }} c ON a.date = c.date
+    b.number_of_ratings,
+    b.average_rating,
+    b.nr_tickets_first_reply_time,
+    b.first_reply_time,
+    b.nr_missed_calls,
+    b.nr_inbound_calls,
+    b.missed_call_ratio,
+    b.retention_success,
+    b.retention_attempts,
+    b.nr_tickets_resolution_time,
+    b.resolution_time
+FROM {{ ref('rpt_activation_daily_kpis') }} a
+LEFT JOIN {{ ref('rpt_customer_service_daily_kpis') }} b ON a.date = b.date
