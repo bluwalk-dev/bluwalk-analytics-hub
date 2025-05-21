18b1edc243d616aaa24623f4f885a5811482fd5a
mpinto@bluwalk.com <mpinto@bluwalk.com>
2025-05-21 18:15:40 +0000

    fix on sales performance metrics

--- models/reporting/aggregates/sales/agg_sales_monthly_bonus.sql
+++ models/reporting/aggregates/sales/agg_sales_monthly_bonus.sql
@@ -1,17 +1,18 @@
 select
-    a.year_month,
+    c.year_month,
     a.bonus,
     COALESCE(b.first_payment_total, 0) as insurance_deal_value
-from (
+from {{ ref('util_month_intervals') }} c
+left join (
     SELECT 
         year_month, 
         SUM(bonus) bonus
     FROM {{ ref('agg_sales_monthly_pipeline_bonus') }}
     GROUP BY year_month
-) a
+) a on c.year_month = a.year_month
 LEFT JOIN (
     SELECT * 
     FROM {{ ref('agg_sm_monthly_insurance_agent_deal_first_payment') }}
     WHERE agent_name = 'Miguel Almeida'
-) b on a.year_month = b.year_month
+) b on c.year_month = b.year_month
 ORDER BY year_month DESC
