098d4af6176682a3ba6472d92d0bf8d42a237d7d
mpinto-bluwalk <67169217+mpinto-bluwalk@users.noreply.github.com>
2025-05-22 21:37:44 +0100

    updates on exposures

---
+++ models/marts/fleet/rpt_fleet_insurance_list_for_vouchers.sql
@@ -0,0 +1,13 @@
+SELECT 
+    insurance_policy_number, 
+    insurer_name, 
+    vehicle_plate, 
+    contact_full_name
+FROM {{ ref('dim_insurance_policies') }} a
+LEFT JOIN {{ ref('dim_vehicles') }} b on a.insurance_vehicle_id = b.vehicle_id
+LEFT JOIN {{ ref('dim_contacts') }} c on a.insurance_contact_id = c.contact_id
+WHERE 
+  insurance_renewal_date > date_sub(current_date(), INTERVAL 60 DAY) and
+  insurance_state = 'active' and
+  insurance_type_line = 'auto' and
+  vehicle_plate is not null
