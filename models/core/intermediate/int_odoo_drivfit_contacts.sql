d7bd6c294007efbbb992803de0af948498635b15
mpinto@bluwalk.com <mpinto@bluwalk.com>
2025-06-05 09:08:00 +0000

    rpt_fleet_insurance_commission

--- models/core/intermediate/int_odoo_drivfit_contacts.sql
+++ models/core/intermediate/int_odoo_drivfit_contacts.sql
@@ -6,5 +6,6 @@ SELECT
   vat as contact_vat,
   street as contact_address,
   zip as contact_zip,
-  city as contact_city
+  city as contact_city,
+  parent_id as contact_parent_id
 FROM {{ ref('stg_odoo_drivfit__res_partners') }}
