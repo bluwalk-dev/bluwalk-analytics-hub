5f9b8183aa1d94f987be6cf83ab2daff7cb4186b
mpinto@bluwalk.com <mpinto@bluwalk.com>
2025-08-28 17:59:24 +0000

    new evio sessions model

--- models/core/base/base_evio_sessions.sql
+++ models/core/base/base_evio_sessions.sql
@@ -6,7 +6,7 @@ WITH sessions AS (
                 PARTITION BY transaction_id 
                 ORDER BY load_timestamp DESC
             ) AS __row_number
-        FROM {{ ref("stg_evio__charging_sessions") }} 
+        FROM {{ ref("stg_evio__sessions") }} 
         )
     WHERE __row_number = 1
 )
