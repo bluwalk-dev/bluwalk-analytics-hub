de45f0a1e9e8d3f2bc5474c90fc5a483da054377
mpinto@bluwalk.com <mpinto@bluwalk.com>
2025-08-30 01:08:17 +0000

    fix on lease_billing

--- models/interface/cloud_functions/intf_fleet_sync_lease_billing__debits_grouped.sql
+++ models/interface/cloud_functions/intf_fleet_sync_lease_billing__debits_grouped.sql
@@ -1,11 +1,12 @@
 select 
   a.opl_conditions_id as conditions_id,
   a.product_id,
+  payment_cycle,
   SUM(amount) AS sum_debit
-  payment_cycle
 from {{ ref('fct_fleet_billable_items') }} a
 left join {{ ref('fct_fleet_rental_contracts') }} b on a.contract_id = b.vehicle_contract_id
 where 
   vehicle_contract_length = 'mid-term' and 
   status = 'invoiced' and
   opl_conditions_id is not null
+group by conditions_id, product_id, payment_cycle
