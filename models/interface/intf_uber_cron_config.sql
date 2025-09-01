72a77a3f219f3a50d813184bc79edd9a7a81e822
mpinto@bluwalk.com <mpinto@bluwalk.com>
2025-09-01 21:52:15 +0000

    remove some uber staging models

--- models/interface/intf_uber_cron_config.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-    login_id,
-    org_name,
-    report_type,
-    last_loaded
-FROM {{ ref('stg_uber__cron_config') }}
--- models/reporting/dimensions/dim_partners_logins.sql
+++ models/reporting/dimensions/dim_partners_logins.sql
@@ -12,12 +12,12 @@ LEFT JOIN {{ ref('dim_locations') }} b ON a.location_id = b.location_id
 UNION ALL
 
 SELECT DISTINCT
-    a.partner_key,
-    a.sales_partner_id,
-    a.org_alt_name org_name,
-    a.location_id,
+    'a6bedefed0135d2ca14b8a1eb7512a50' as partner_key,
+    1 as sales_partner_id,
+    a.org_name,
+    a.odoo_location_id as location_id,
     b.location_name,
-    a.login_id,
-    a.sales_tax_rate
-FROM {{ ref('stg_uber__cron_config') }} a
-LEFT JOIN {{ ref('dim_locations') }} b ON a.location_id = b.location_id
+    a.org_id as login_id,
+    6 as sales_tax_rate
+FROM `bluwalk-analytics-hub.staging.stg_uber_organizations` a
+LEFT JOIN {{ ref('dim_locations') }} b ON a.odoo_location_id = b.location_id
