e87557d484156e15886f7640a8eb123aed1f1047
mpinto@bluwalk.com <mpinto@bluwalk.com>
2025-09-05 09:46:08 +0000

    fixes

--- models/core/intermediate/int_fleet_booking_weekly_per_create_date.sql
+++ models/core/intermediate/int_fleet_booking_weekly_per_create_date.sql
@@ -3,6 +3,6 @@ select
   count(*) new_bookings
 from {{ ref('stg_odoo_drivfit__bookings') }} a
 left join {{ ref('util_calendar') }} b on cast(a.create_date as date) = b.date
-where booking_type = 'new' and ((active = false and state = 'close') OR (active = true))
+where booking_type = 'new' and state IN ('open', 'close', 'confirm')
 group by year_week
 order by year_week desc
