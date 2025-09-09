8225cf5d232fd1e5f9f2557bc64ecc1aec1bec58
mpinto@bluwalk.com <mpinto@bluwalk.com>
2025-09-09 14:00:24 +0000

    delete mercadao and free now sources

--- models/core/base/base_mercadao_orders.sql
+++ models/core/base/base_mercadao_orders.sql
@@ -10,21 +10,8 @@ SELECT
     a.date,                        -- Date of the order from the subquery 'a'
     a.nr_orders,                   -- Number of orders from the subquery 'a'
     a.order_type,                  -- Type of the order from the subquery 'a'
-FROM ( 
-    -- Subquery to get the latest order log for each partner account, date, and order type
-    SELECT 
-        *, 
-        ROW_NUMBER() OVER (
-            PARTITION BY partner_account_uuid, date, order_type 
-            ORDER BY extraction_timestamp DESC
-        ) AS __row_number
-    FROM {{ ref("stg_mercadao__order_log") }} 
-) a
-LEFT JOIN {{ ref('dim_partners_accounts') }} b 
-    ON a.partner_account_uuid = b.partner_account_uuid  -- Join with partners accounts dimension
-LEFT JOIN {{ ref('dim_users') }} c 
-    ON c.contact_id = b.contact_id                       -- Join with users dimension
-WHERE 
-    __row_number = 1 AND                                  -- Filter to get the latest record per group
-    date < current_date                                  -- Filter to get records before the current date
-ORDER BY date DESC                                       -- Order results by date in descending order
+FROM bluwalk-analytics-hub.staging.stg_mercadao_orders a
+LEFT JOIN {{ ref('dim_partners_accounts') }} b ON a.partner_account_uuid = b.partner_account_uuid
+LEFT JOIN {{ ref('dim_users') }} c ON c.contact_id = b.contact_id             
+WHERE date < current_date
+ORDER BY date DESC
