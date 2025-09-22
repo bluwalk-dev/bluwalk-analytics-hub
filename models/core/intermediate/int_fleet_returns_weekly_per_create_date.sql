95feba74f3a320be5fc9f4e5675fca04f74cdf90
mpinto@bluwalk.com <mpinto@bluwalk.com>
2025-09-22 16:52:46 +0000

    delete models

--- models/core/intermediate/int_fleet_returns_weekly_per_create_date.sql
+++
@@ -1,8 +0,0 @@
-select 
-  b.year_week,
-  count(*) new_returns
-from bluwalk-analytics-hub.staging.stg_odoo_flt_booking_returns a
-left join bluwalk-analytics-hub.core.ref_calendar b on cast(a.create_date as date) = b.date
-where booking_return_type = 'definitive' and booking_return_state IN ('confirm','close')
-group by year_week
-order by year_week desc
--- models/marts/sales/rpt_sales_monthly_pipeline_kpis.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    a.year_month,
-    b.pipeline_name,
-    b.bonus
-FROM {{ ref('util_month_intervals') }} a
-LEFT JOIN {{ ref('agg_sales_monthly_pipeline_bonus') }} b ON a.year_month = b.year_month
-WHERE a.year_month <= CAST(FORMAT_DATE('%Y%m', CURRENT_DATE()) AS INT64)
-ORDER BY a.year_month DESC
