ad80c44141b1fedda96a78a9c501eb1c5d53a554
mpinto@bluwalk.com <mpinto@bluwalk.com>
2025-10-22 22:55:33 +0000

    clean models

--- models/core/base/base_hubspot_users.sql
+++ models/core/base/base_hubspot_users.sql
@@ -8,9 +8,9 @@ SELECT
     c.id hubspot_team_id,
     c.name hubspot_team_name,
     d.owner_id hubspot_owner_id
-FROM {{ ref("stg_hubspot__users") }} a
-LEFT JOIN {{ ref("stg_hubspot__teams") }} c on a.primary_team_id = c.id
-LEFT JOIN {{ ref("stg_hubspot__owners") }} d on a.email = d.email
+FROM bluwalk-analytics-hub.staging.stg_hubspot_users a
+LEFT JOIN bluwalk-analytics-hub.staging.stg_hubspot_teams c on a.primary_team_id = c.id
+LEFT JOIN bluwalk-analytics-hub.staging.stg_hubspot_owners d on a.email = d.email
 
 UNION ALL
 
