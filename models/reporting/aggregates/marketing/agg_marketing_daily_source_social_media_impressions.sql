0f99d35cf5c973fd7052a4542327f17dc0faebe6
mpinto@bluwalk.com <mpinto@bluwalk.com>
2025-11-20 13:34:57 +0000

    many model removals

--- models/reporting/aggregates/marketing/agg_marketing_daily_source_social_media_impressions.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    date, 
-    'Facebook' source, 
-    page_content_activity total_activity,
-    page_fans total_followers
-FROM {{ ref('stg_facebook__impressions') }}
-ORDER BY date DESC, source
--- models/reporting/aggregates/marketing/agg_marketing_monthly_social_media_impressions.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    b.year_month,
-    SUM(total_activity) total_activity,
-    ROUND(AVG(total_followers),0) total_followers,
-    ROUND(SUM(total_activity)/AVG(total_followers), 4) monthly_engagement_rate
-FROM {{ ref('agg_marketing_daily_source_social_media_impressions') }} a
-LEFT JOIN {{ ref('util_calendar') }} b on a.date = b.date
-GROUP BY year_month
-ORDER BY year_month DESC
