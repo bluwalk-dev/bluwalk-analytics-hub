c76251a1f59c65c3342b87482483e9408f67ab93
mpinto@bluwalk.com <mpinto@bluwalk.com>
2025-12-15 17:21:46 +0000

    remove old aircall connector from fivcetran

--- models/staging/aircall/stg_aircallV3__numbers.sql
+++ models/staging/aircall/stg_aircallV3__numbers.sql
@@ -1,16 +1,1 @@
-with
-
-source as (
-    SELECT *
-    FROM {{ source('aircall_v3', 'numbers') }}
-),
-
-transformation as (
-
-    select
-        *
-    from source
-
-)
-
-select * from transformation
+select * from bluwalk-analytics-hub.source_aircall.numbers
